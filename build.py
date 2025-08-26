6e62b902b8315b2232ea2846fd6a5b9b266cd0ca
resserops <resserops@gmail.com>
2025-08-26 20:47:34 +0800

    build: support debug and release build type

--- build.py
+++ build.py
@@ -1,8 +1,17 @@
 #! /usr/bin/env python3
 import os
+import argparse
+
+parser = argparse.ArgumentParser(description='build')
+
+build_type_group = parser.add_mutually_exclusive_group()
+build_type_group.add_argument('-d', '--debug', dest='build_type', action='store_const', const='Debug', default='Release')
+build_type_group.add_argument('-r', '--release', dest='build_type', action='store_const', const='Release', default='Release')
+
+args = parser.parse_args()
 
 if not os.path.exists('build'):
     os.mkdir('build')
 os.chdir('build')
-os.system('cmake -DENABLE_TEST=ON ..')
+os.system(f'cmake -DCMAKE_BUILD_TYPE={args.build_type} -DENABLE_TEST=ON ..')
 os.system('cmake --build .')
