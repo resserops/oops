2e87de71706f0739088e7972ee41f555b049900a
resserops <resserops@gmail.com>
2025-09-03 10:19:44 +0800

    feat(profiling/system_info): add system_info submodule

---
+++ module/profiling/test/test_system_info.cpp
@@ -0,0 +1,20 @@
+#include <thread>
+
+#include "gtest/gtest.h"
+
+#include "oops/system_info.h"
+
+using namespace oops;
+
+TEST(ProfilingSystemInfo, Status) {
+    std::cout << status::Get();
+}
+
+TEST(ProfilingSystemInfo, Status2) {
+    std::cout << status::Get(status::Field::VM_RSS | status::Field::VM_HWM);
+}
+
+TEST(ProfilingSystemInfo, Lscpu) {
+    std::cout << lscpu::Get();
+}
+
