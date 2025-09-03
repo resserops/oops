aee0f91537207686c2416d0d8bf0746f4ba975e1
resserops <resserops@gmail.com>
2025-09-03 10:25:56 +0800

    feat(common/member): add member submodule

---
+++ module/common/include/oops/member.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define OOPS_HAS_MEMBER(class, member)                                             \
+    []() constexpr {                                                               \
+        constexpr auto trait{::oops::impl::MemberTrait(                            \
+            [](auto &&x) ->decltype((x.member), std::true_type{}) { return {}; },  \
+            [](...) { return std::false_type{}; })};                               \
+        return decltype(trait(std::declval<class>()))::value;                      \
+    } ()
+
+#ifndef HAS_MEMBER
+#define HAS_MEMBER(class, member) OOPS_HAS_MEMBER(class, member)
+#endif
+
+namespace oops {
+namespace impl {
+template <typename T, typename F>
+struct MemberTrait : public T, public F {
+    using T::operator();
+    using F::operator();
+    constexpr MemberTrait(T &&t, F &&f) : T(std::move(t)), F(std::move(f)) {}
+};
+}
+}
