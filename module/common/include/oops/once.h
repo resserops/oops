c4ef534ad8e76a8497bd105adf740ae81e39ebfa
resserops <resserops@gmail.com>
2025-09-11 19:42:02 +0800

    perf(common/once): improve performance of specific branches in Only and Every

--- module/common/include/oops/once.h
+++ module/common/include/oops/once.h
@@ -2,7 +2,7 @@
 
 #include <cstddef>
 
-#define OOPS_ONLY(n) if (::oops::impl::Only<n>([]{}))
+#define OOPS_ONLY(n) if (::oops::detail::Only<n>::F([]{}))
 #ifndef ONLY
 #define ONLY(n) OOPS_ONLY(n)
 #endif
@@ -17,30 +17,66 @@
 #define TWICE() OOPS_TWICE()
 #endif
 
-#define OOPS_EVERY(n) if (::oops::impl::Every<n>([]{}))
+#define OOPS_EVERY(n) if (::oops::detail::Every<n>::F([]{}))
 #ifndef EVERY
 #define EVERY(n) OOPS_EVERY(n)
 #endif
 
 namespace oops {
-namespace impl {
-template <size_t N, typename F>
-bool Only(F &&) {
-    static size_t count{0};
-    if (count < N) {
-        ++count;
+namespace detail {
+template <size_t N>
+struct Only {
+    template <typename Lambda>
+    static bool F(Lambda &&) {
+        static size_t count{0};
+        if (count < N) {
+            ++count;
+            return true;
+        }
+        return false;
+    }
+};
+
+template <>
+struct Only<1> {
+    template <typename Lambda>
+    static bool F(Lambda &&) {
+        static bool done{false};
+        if (!done) {
+            done = true;
+            return true;
+        }
+        return false;
+    }
+};
+
+template <>
+struct Only<0> {
+    template <typename Lambda>
+    constexpr static bool F(Lambda &&) {
+        return false;
+    }
+};
+
+template <size_t N>
+struct Every {
+    static_assert(N > 0);
+    template <typename Lambda>
+    static bool F(Lambda &&) {
+        static size_t count{0};
+        if (count >= N) {
+            count = 0;
+        }
+        return count++ == 0;
+    }
+};
+
+template <>
+struct Every<1> {
+    template <typename Lambda>
+    constexpr static bool F(Lambda &&) {
         return true;
     }
-    return false;
-}
-
-template <size_t N, typename F>
-bool Every(F &&) {
-    static size_t count{0};
-    if (count >= N) {
-        count = 0;
-    }
-    return count++ == 0;
-}
+};
 }
 }
