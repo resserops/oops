4bc8dd342910ad98bbdec1bdae6ec171687bc08e
resserops <resserops@gmail.com>
2025-12-12 23:49:17 +0800

    build: optimize CMake configuration

--- module/matrix/test/test_coo.cpp
+++ module/matrix/test/test_coo.cpp
@@ -5,17 +5,8 @@
 
 using namespace oops;
 
-constexpr const char *MTX{
-    R"(%%MatrixMarket matrix coordinate real symmetric
-3 3 4
-1 1 3.2
-2 2 5.4
-3 1 2.0
-3 3 1.1
-)"};
-
 TEST(Coo, ReadCoo) {
-    std::istringstream iss(MTX);
+    std::ifstream iss(std::string(CASE_DIR) + "/crs.mtx");
     CooVar coo = ReadMatrixMarket(iss);
     std::visit([](const auto &coo) { std::cout << coo.M() << " " << coo.N() << " " << coo.Nnz() << std::endl; }, coo);
 }
