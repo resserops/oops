61f75dd41632798b7bf6be96f32ff338e969876e
resserops <resserops@gmail.com>
2026-03-09 19:24:43 +0800

    build: integrate argparse library using git submodule

--- third_party/cmake/googletest.cmake
+++ third_party/cmake/googletest.cmake
@@ -1,4 +1,6 @@
-if(NOT TARGET gtest)
-    execute_process(COMMAND git submodule update --init --recursive ${oops_3rd_dir}/googletest)
-    add_subdirectory(${oops_3rd_dir}/googletest)
+set(googletest_dir ${oops_3rd_dir}/googletest)
+if(NOT EXISTS "${googletest_dir}/CMakeLists.txt")
+    execute_process(COMMAND git submodule update --init --recursive ${googletest_dir})
 endif()
+
+add_subdirectory(${googletest_dir})
