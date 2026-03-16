507f410a4701942cd13e78b6b088e41d10949264
resserops <resserops@gmail.com>
2026-03-16 11:06:00 +0800

    build: integrate fmt third-party library using git submodule

--- third_party/cmake/googletest.cmake
+++ third_party/cmake/googletest.cmake
@@ -1,6 +1,7 @@
-set(googletest_dir ${oops_3rd_dir}/googletest)
-if(NOT EXISTS "${googletest_dir}/CMakeLists.txt")
-    execute_process(COMMAND git submodule update --init --recursive ${googletest_dir})
+# googletest-1.17.0 (52eb8108)
+set(lib_dir ${oops_3rd_dir}/googletest)
+if(NOT EXISTS "${lib_dir}/CMakeLists.txt")
+    execute_process(COMMAND git submodule update --init --recursive ${lib_dir})
 endif()
 
-add_subdirectory(${googletest_dir})
+add_subdirectory(${lib_dir})
