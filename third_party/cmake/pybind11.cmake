caeba82db82a1c67828c43883088983df0f68517
resserops <resserops@gmail.com>
2026-05-23 00:40:57 +0800

    feat(profiling/system_info): add smaps parser part 2

--- third_party/cmake/pybind11.cmake
+++ third_party/cmake/pybind11.cmake
@@ -4,4 +4,5 @@ if(NOT EXISTS "${lib_dir}/CMakeLists.txt")
     execute_process(COMMAND git submodule update --init --recursive ${lib_dir})
 endif()
 
+set(PYBIND11_FINDPYTHON NEW)    # 设置pybind11使用现代查找模式
 add_subdirectory(${lib_dir})
